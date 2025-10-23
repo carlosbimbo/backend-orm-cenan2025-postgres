cdf25a4c607efbfe3d0f3ca98e7c8439650e3b2f
cabs <carlosbars8@gmail.com>
2025-10-23 13:31:54 -0500

    saveOrupdUserapp firstin

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -19,7 +19,7 @@ router.get("/findetapagesta/:id", authMiddleware.verifyToken, t05etapagestaContr
 router.get("/findregisevent/:id", authMiddleware.verifyToken, t05regiseventController.getT05regisevent);
 
 /*for user appcenan*/
-router.post("/newuserfirstsign", authController.createUserapp);
+router.post("/newuserfirstsign", authController.saveOrupdUserapp);
 router.post("/newuserapp", authMiddleware.verifyToken, authController.createUserapp);
 router.post("/updateuserapp", authMiddleware.verifyToken, authController.udpUserapp);
 router.post("/saveuserapp", authMiddleware.verifyToken, authController.saveOrupdUserapp);

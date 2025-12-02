07815151f2b1619cca37c251d48d011d12b4c84a
cabs <carlosbars8@gmail.com>
2025-12-02 14:28:09 -0500

    add download foto v3

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@
  * Acceso principal al proyecto
  */
 
-/*
 const express = require('express')
 const configureMiddleware = require("../src/config/middleware");
 const app = express()
@@ -16,29 +15,4 @@ app.use(router)
 app.listen(8099, () => {
   console.log("Server is running!\nAPI 8099" )
 })
-*/
-
-const express = require("express");
-const configureMiddleware = require("../src/config/middleware");
-const path = require("path");
-
-const app = express();
-
-// Middlewares globales
-configureMiddleware(app);
-
-// 🔥 PUBLICAR FOTOSCAPTURE
-app.use(
-  "/cenan2025/fotoscapture",
-  express.static(path.join(__dirname, "fotoscapture"))
-);
-
-// Rutas de la API
-const router = require("./routes/authRoutes");
-app.use("/cenan2025", router);
-
-// Servidor
-app.listen(8099, () => {
-  console.log("Server is running!\nAPI 8099");
-});
 

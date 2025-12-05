443c3a3dce5f6b84f6012ee4d18d222c0875f8b9
cabs <carlosbars8@gmail.com>
2025-12-05 18:47:47 -0500

    add download foto v7

--- src/index.js
+++ src/index.js
@@ -3,12 +3,18 @@
  */
 
 const express = require('express')
+const path = require('path');
 const configureMiddleware = require("../src/config/middleware");
 const app = express()
 
 const router = require('./routes/authRoutes')
 
 configureMiddleware(app);
+
+app.use(
+  "/cenan2025/fotoscapture",
+  express.static(path.join(__dirname, "fotoscapture"))
+);
 
 app.use(router)
 

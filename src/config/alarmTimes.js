577a47b462accbaff5b56d48cae9282af2eb3e13
cabs <carlosbars8@gmail.com>
2025-12-17 13:07:58 -0500

    add sonidos app android v2

--- src/config/alarmTimes.js
+++ src/config/alarmTimes.js
@@ -15,7 +15,8 @@ module.exports = [
     { idalar: 5, hour: 12, minute: 14, message: "⏰ Mami toma mi hierro por favor", sound: "alerta2" },
   ];
   */
- 
+ /*
+ //Finalmente probe con esta casusitica 17122025
   module.exports = [
     { idalar: 1, hour: 12, minute: 56, message: "⏰ Mami ya toca mi hierro", sound: "alerta_uno", channelId: "v8_alerta_uno" },
     { idalar: 2, hour: 12, minute: 58, message: "⏰ Mami toma mi hierro por favor", sound: "alerta_dos", channelId: "v8_alerta_dos" },
@@ -23,4 +24,11 @@ module.exports = [
     { idalar: 4, hour: 13, minute: 2, message: "⏰ Mami ya toca mi hierro", sound: "alerta_uno", channelId: "v8_alerta_uno" },
     { idalar: 5, hour: 13, minute: 4, message: "⏰ Mami toma mi hierro por favor", sound: "alerta_dos", channelId: "v8_alerta_dos" },
   ];
-  
+  */ 
+  module.exports = [
+    { idalar: 1, hour: 10, minute: 30, message: "⏰ Mami ya toca mi hierro", sound: "alerta_uno", channelId: "v8_alerta_uno" },
+    { idalar: 2, hour: 11, minute: 0, message: "⏰ Mami toma mi hierro por favor", sound: "alerta_dos", channelId: "v8_alerta_dos" },
+    { idalar: 3, hour: 17, minute: 30, message: "⏰ Gracias mamita, tenemos más hierro", sound: "alerta_tres", channelId: "v8_alerta_tres" },
+    { idalar: 4, hour: 15, minute: 30, message: "⏰ Mami ya toca mi hierro", sound: "alerta_uno", channelId: "v8_alerta_uno" },
+    { idalar: 5, hour: 16, minute: 0, message: "⏰ Mami toma mi hierro por favor", sound: "alerta_dos", channelId: "v8_alerta_dos" },
+  ];
